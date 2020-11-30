d70dfb369aecadf720531e6f02b457b5ae1bd601
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-11-30 20:43:44 +0200

    Fix sample test

--- tests/services/summaryService_test.js
+++ tests/services/summaryService_test.js
@@ -3,5 +3,5 @@ import app from "../../app.js";
 
 Deno.test("GET request to / should return correct content", async () => {
   const testClient = await superoak(app);
-  await testClient.get("/").expect("Hello world!");
+  await testClient.get("/").expect("");
 });
