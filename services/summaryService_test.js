81c9e634e52ba2ff33642267f8f3810abb9c9d1d
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-12-10 11:27:17 +0200

    Fix GH Actions

---
+++ services/summaryService_test.js
@@ -0,0 +1,15 @@
+import { superoak, assertEquals } from '../deps.js'
+import app from "../app.js";
+
+Deno.test({
+  name: "GET request to / should return correct content",
+  async fn () {
+    const testClient = await superoak(app);
+    const response = await testClient.get("/")
+    // assertEquals("Hello", "Hello")
+    assertEquals(response.statusCode, 200)
+    Deno.exit(0)
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+});
--- tests/services/summaryService_test.js
+++
@@ -1,7 +0,0 @@
-import { superoak } from "https://deno.land/x/superoak@2.3.1/mod.ts";
-import app from "../../app.js";
-
-Deno.test("GET request to / should return correct content", async () => {
-  const testClient = await superoak(app);
-  await testClient.get("/").expect("");
-});
